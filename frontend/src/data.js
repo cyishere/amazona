1eb0fe2a3e8ec8d0fe5faa20bc072f4a7671a90f
cyishere <cyishere@gmail.com>
2020-09-13 12:46:13 +0800

    Render products in jsx

---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+export default {
+  products: [
+    {
+      name: "Black Dress",
+      category: "Dresses",
+      image: "/images/d1.jpeg",
+      price: 60,
+      brand: "Armani",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      name: "White Dress",
+      category: "Dresses",
+      image: "/images/d2.jpeg",
+      price: 50,
+      brand: "Alexander McQueen",
+      rating: 4.2,
+      numReviews: 8,
+    },
+    {
+      name: "Best Pants",
+      category: "Pants",
+      image: "/images/d3.jpeg",
+      price: 70,
+      brand: "Cate Blanchett",
+      rating: 4.7,
+      numReviews: 7,
+    },
+    {
+      name: "White Pants",
+      category: "Pants",
+      image: "/images/d4.jpeg",
+      price: 80,
+      brand: "Cate Blanchett",
+      rating: 4.9,
+      numReviews: 15,
+    },
+  ],
+};
