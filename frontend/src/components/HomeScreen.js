93925bc95f8bcc4d812487b5b14fb3a3a082f37d
cyishere <cyishere@gmail.com>
2020-09-13 17:17:52 +0800

    Manage state with Redux

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -2,34 +2,42 @@
  * @Author: chen yang
  * @Date: 2020-09-13 13:14:01
  * @Last Modified by: chen yang
- * @Last Modified time: 2020-09-13 15:42:37
+ * @Last Modified time: 2020-09-13 16:50:41
  */
 import React from "react";
 import { Link } from "react-router-dom";
 
-const HomeScreen = ({ products }) => {
+const HomeScreen = ({ products, loading, error }) => {
   return (
-    <ul className="products">
-      {products.map((product) => (
-        <li key={product._id}>
-          <div className="product">
-            <Link to={`/product/${product._id}`}>
-              <img
-                className="product-image"
-                src={product.image}
-                alt={product.name}
-              />
-              <div className="product-name">{product.name}</div>
-            </Link>
-            <div className="product-brand">{product.brand}</div>
-            <div className="product-price">${product.price}</div>
-            <div className="product-rating">
-              {product.rating} Stars ({product.numReviews} Reviews)
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <ul className="products">
+          {products.map((product) => (
+            <li key={product._id}>
+              <div className="product">
+                <Link to={`/product/${product._id}`}>
+                  <img
+                    className="product-image"
+                    src={product.image}
+                    alt={product.name}
+                  />
+                  <div className="product-name">{product.name}</div>
+                </Link>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price">${product.price}</div>
+                <div className="product-rating">
+                  {product.rating} Stars ({product.numReviews} Reviews)
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
---
+++ frontend/src/constants/productConstants.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: chen yang
+ * @Date: 2020-09-13 16:29:46
+ * @Last Modified by: chen yang
+ * @Last Modified time: 2020-09-13 16:31:14
+ */
+export const PRODUCT_LIST_REQUEST = "PRODUCT_LIST_REQUEST";
+export const PRODUCT_LIST_SUCCESS = "PRODUCT_LIST_SUCCESS";
+export const PRODUCT_LIST_FAIL = "PRODUCT_LIST_FAIL";
